f8ee70fa033612ec529e0a5426f9faaf5468c62e
cnvorous <cnvorous@gmail.com>
2022-02-25 20:24:55 +0000

    Get Fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,20 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getData: ()=> {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/cnvorous")
+					.then(response => response.json())
+					.then(resultJsonified => {
+						setStore({contacts: resultJsonified});  // becuase get method 
+					})
+					.catch(error => console.log(error));
+			}
 		}
 	};
 };
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js";
 
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
 export const Contacts = () => {
+	const {store,actions}= useContext(Context); // this doesnt change its standard 
 	const [state, setState] = useState({
 		showModal: false
 	});
@@ -19,10 +21,12 @@ export const Contacts = () => {
 				</p>
 				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => setState({ showModal: true })} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
+						{store.contacts.map((item, index)=>{
+							return(
+								<ContactCard history= {item} key={index} onDelete={() => setState({ showModal: true })} />
+							)
+						}
+						)}
 					</ul>
 				</div>
 			</div>
